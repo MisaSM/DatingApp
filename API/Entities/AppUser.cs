036d39b3c51b3a994b602891f232cced6481beef
MisaSM <misaelsarabiamolina@gmail.com>
2023-05-17 16:04:01 -0700

    Finished section 15!

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -29,5 +29,10 @@ namespace API.Entities
         public List<UserLike> LikedByUsers {get; set;}
 
         public List<UserLike> LikedUsers {get; set;}
+
+        public List<Message> MessagesSent {get; set;}
+
+        public List<Message> MessagesReceived {get; set;}
+
     }
 }
---
+++ client/src/app/_resolvers/member-detailed.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { MembersService } from '../_services/members.service';
+import { Member } from '../_models/Member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberDetailedResolver implements Resolve<Member> {
+  constructor(private memberService: MembersService) 
+  {
+
+  }
+  resolve(route: ActivatedRouteSnapshot): Observable<Member> {
+    return this.memberService.getMember(route.paramMap.get('username')!) 
+  }
+}
